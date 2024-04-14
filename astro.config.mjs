b0e4475a418e2c7269e19ae4c4fc72ab66fc3d5e
Rowan Murray <rowanomurray@gmail.com>
2024-04-14 13:57:48 +0100

    chore: lighthouse accessibility recommendations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.rowmur.com",
-  integrations: [tailwind()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
